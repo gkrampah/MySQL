4c39622d9fd309deef6af1df544700346fed2dab
gkrampah <godkram1991@gmail.com>
2023-08-16 18:25:19 +0200

    updated

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MySQL
 Some salient functions for manipulating databases (using MySQL - Structured Query Language)
+
+Used MySQL workbench for accessing databases
---
+++ Sql_commands.sql
@@ -0,0 +1,14 @@
+#SELECT * FROM moviesdb.movie_actor;
+#SELECT title, industry FROM moviesdb.movies;
+use moviesdb;
+#SELECT * FROM movies;
+#SELECT title, industry FROM movies;
+#SELECT * FROM movies WHERE industry='Bollywood';
+#SELECT COUNT(*) FROM movies WHERE industry='Bollywood';
+#SELECT distinct industry FROM movies;
+#SELECT * FROM movies WHERE title LIKE '%THOR%';
+#SELECT * FROM movies WHERE studio="";
+#SELECT title, release_year FROM movies WHERE studio="Marvel Studios";
+#SELECT * FROM movies WHERE title LIKE '%Avenger%';
+#SELECT release_year FROM movies WHERE title="The Godfather";
+SELECT distinct studio FROM movies  WHERE industry="Bollywood";
